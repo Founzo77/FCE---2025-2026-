7c189dba506f02e5619d05ed2a4c9fc7c47db3a1
Founzo <timothe.kruk51@gmail.com>
2026-05-20 18:39:36 +0200

    Last Main Version

--- application/main.cpp
+++ application/main.cpp
@@ -61,9 +61,9 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
     config.m_duration = 5;
     //fce::Benchmark mainWindow(hInstance, 1500, 1500, "scene/prototype_benchmark.xml", config);
 
-    fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/prototype.xml");
+    //fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/prototype.xml");
     //fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/prototype_no_volume.xml");
-    //fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/cornell-box.xml");
+    fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/cornell-box.xml");
 
     //fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/prototype_barney.xml");
     //fce::MainWindow mainWindow(hInstance, 1500, 1500, "scene/game_shinjuku_gojo.xml");
